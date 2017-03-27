7aa651edc1549c27cef8e9513dc4ab3373a93e5f
Conner, Justin <jdconner@gannett.com>
2017-03-27 02:45:04 -0400

    Display first 10 transactions

--- client/app/components/levelMoney/levelMoney.controller.js
+++ client/app/components/levelMoney/levelMoney.controller.js
@@ -1,7 +1,23 @@
+import _ from 'lodash';
+
+const token = 'D03ABBFA004747E07842145195EC7CFC';
+const apiToken = 'AppTokenForInterview';
+const uid = 1110590645;
+const args = {token, uid, 'api-token': apiToken};
+
 class LevelMoneyController {
-  constructor() {
-    this.name = 'levelMoney';
-  }
+    constructor($http) {
+        _.assign(this, {$http});
+        this.GetAllTransactions();
+    }
+
+    GetAllTransactions() {
+        this.$http
+            .post('https://2016.api.levelmoney.com/api/v2/core/get-all-transactions', {args})
+            .then(res => this.data = _.get(res, 'data.transactions'));
+    }
 }
 
-export default LevelMoneyController;
+
+
+export default ['$http', LevelMoneyController];
