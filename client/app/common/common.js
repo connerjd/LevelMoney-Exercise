2ce7645ba46518b06506423c1bac2cdb276abc72
Conner, Justin <jdconner@gannett.com>
2017-03-27 01:23:21 -0400

    Initial Commit

--- client/app/common/common.js
+++ client/app/common/common.js
@@ -1,13 +1,7 @@
 import angular from 'angular';
-import Navbar from './navbar/navbar';
-import Hero from './hero/hero';
-import User from './user/user';
 
-let commonModule = angular.module('app.common', [
-  Navbar,
-  Hero,
-  User
-])
+
+let commonModule = angular.module('app.common', [])
   
 .name;
 
--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
   entry: {},
   module: {
     loaders: [
-       { test: /\.js$/, exclude: [/app\/lib/, /node_modules/], loader: 'ng-annotate!babel' },
+       { test: /\.js$/, exclude: [/app\/lib/, /node_modules/], loader: 'ng-annotate!babel?plugins[]=lodash' },
        { test: /\.html$/, loader: 'raw' },
        { test: /\.(scss|sass)$/, loader: 'style!css!sass' },
        { test: /\.css$/, loader: 'style!css' }
